c7d9d178432bce1468bda06d0418fdd21660a17f
pos0637 <pos0637@163.com>
2018-11-11 23:12:07 +0800

    支持打包。

---
+++ sources/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const electron = require('electron');
+const { app, BrowserWindow, Menu, ipcMain, ipcRenderer } = electron;
+
+
+let isDevelopment = false;
+
+if (isDevelopment) {
+    require('electron-reload')(__dirname, {
+        ignored: /node_modules|[\/\\]\./
+    });
+}
+
+
+var mainWnd = null;
+
+function createMainWnd() {
+    mainWnd = new BrowserWindow({
+        width: 3840,
+        height: 2160,
+        fullscreen: true,
+        icon: 'public/img/app-icon.png'
+    });
+
+    if (isDevelopment) {
+        mainWnd.webContents.openDevTools();
+    }
+
+    mainWnd.loadURL(`file://${__dirname}/dist/index.html`);
+
+    mainWnd.on('closed', () => {
+        mainWnd = null;
+    });
+}
+
+
+app.on('ready', createMainWnd);
+
+app.on('window-all-closed', () => {
+    app.quit();
+});
