5413fc9fda65aa93a72620f3bc734557317ceff5
pos0637 <pos0637@163.com>
2018-12-10 16:15:47 +0800

    完善PCS界面。

--- sources2/src/app/pcs/index.js
+++ sources2/src/app/pcs/index.js
@@ -5,6 +5,8 @@ import Text from '~/app/components/text';
 import Image from '~/app/components/image';
 import Container from '~/app/components/container';
 import Switch from '~/app/components/switch';
+import Progress from '~/app/components/lineProgress';
+import LineChart from '~/app/components/lineChart';
 import { toFixed } from '~/misc/number';
 
 /**
@@ -21,7 +23,23 @@ export default class Pcs extends BaseComponent {
 
     state = {
         电网状态1: 0,
-        电网状态2: 0
+        电网状态2: 0,
+        充电1: 0,
+        放电1: 0,
+        充电2: 0,
+        放电2: 0,
+        日充电电量1: 36000,
+        日放电电量1: 36000,        
+        日充电电量2: 36000,
+        日放电电量2: 36000,
+        日放电功率1: 360,        
+        日放电功率2: 360,
+        直流电压1: 220,
+        直流电压2: 220,
+        直流电流1: 50,
+        直流电流2: 50,
+        逆变电流1: 50,
+        逆变电流2: 50
     }
 
     componentDidMount() {
@@ -58,7 +76,88 @@ export default class Pcs extends BaseComponent {
                 </Container>
 
                 <Container left={916} top={378} background={require("./images/box3.png")}>
-                    <Text left={1022} top={428} value="储能电站" font="SourceHanSansSC-Medium" fontSize={48} />                    
+                    <Text left={1022} top={428} value="1#储能电站" font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <Switch left={1024} top={550} src1={require("./images/charging0.png")} src2={require("./images/charging1.png")} value={this.state.充电1 === 0} />
+                    <Switch left={1474} top={550} src1={require("./images/discharging0.png")} src2={require("./images/discharging1.png")} value={this.state.放电1 === 0} />
+                </Container>
+                <Container left={1933} top={378} background={require("./images/box3.png")}>
+                    <Text left={2037} top={428} value="2#储能电站" font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <Switch left={2041} top={550} src1={require("./images/charging0.png")} src2={require("./images/charging1.png")} value={this.state.充电2 === 0} />
+                    <Switch left={2491} top={550} src1={require("./images/discharging0.png")} src2={require("./images/discharging1.png")} value={this.state.放电2 === 0} />
+                </Container>
+
+                <Container left={916} top={692} background={require("./images/box3.png")}>
+                    <Text left={1022} top={742} value="日充电电量" font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <Text left={1700} top={866} value={`${toFixed(this.state.日充电电量1, 0)}`} font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <Text left={1700} top={918} value="kWh" font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <LineChart left={1026} top={861} width={637} height={115} color="rgba(68,175,244,0.8)" />
+                </Container>
+                <Container left={1933} top={692} background={require("./images/box3.png")}>
+                    <Text left={2043} top={742} value="日充电电量" font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <Text left={2717} top={866} value={`${toFixed(this.state.日充电电量2, 0)}`} font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <Text left={2717} top={918} value="kWh" font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <LineChart left={2043} top={861} width={637} height={115} color="rgba(68,175,244,0.8)" />
+                </Container>
+
+                <Container left={916} top={1004} background={require("./images/box3.png")}>
+                    <Text left={1022} top={1054} value="日放电电量" font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <Text left={1700} top={1179} value={`${toFixed(this.state.日放电电量1, 0)}`} font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <Text left={1700} top={1229} value="kWh" font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <LineChart left={1026} top={1173} width={637} height={115} color="rgba(68,175,244,0.8)" />
+                </Container>
+                <Container left={1933} top={1004} background={require("./images/box3.png")}>
+                    <Text left={2043} top={1054} value="日放电电量" font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <Text left={2717} top={1179} value={`${toFixed(this.state.日放电电量2, 0)}`} font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <Text left={2717} top={1229} value="kWh" font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <LineChart left={2043} top={1173} width={637} height={115} color="rgba(68,175,244,0.8)" />
+                </Container>
+
+                <Container left={916} top={1316} background={require("./images/box3.png")}>
+                    <Text left={1022} top={1369} value="日放电电量" font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <Text left={1700} top={1494} value={`${toFixed(this.state.日放电功率1, 0)}`} font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <Text left={1700} top={1553} value="w" font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <LineChart left={1026} top={1488} width={637} height={115} color="rgba(68,175,244,0.8)" />
+                </Container>
+                <Container left={1933} top={1316} background={require("./images/box3.png")}>
+                    <Text left={2043} top={1369} value="日放电电量" font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <Text left={2717} top={1494} value={`${toFixed(this.state.日放电功率2, 0)}`} font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <Text left={2717} top={1553} value="w" font="SourceHanSansSC-Medium" fontSize={48} />    
+                    <LineChart left={2043} top={1488} width={637} height={115} color="rgba(68,175,244,0.8)" />
+                </Container>
+
+                <Container left={916} top={1628} background={require("./images/box4.png")}>
+                    <Text left={1024} top={1690} value="直流电压" font="SourceHanSansSC-Medium" fontSize={48} color="rgb(60, 211, 238)" />    
+                    <Text left={1299} top={1690} value="直流电流" font="SourceHanSansSC-Medium" fontSize={48} color="rgb(60, 211, 238)" />    
+                    <Text left={1588} top={1690} value="逆变电流" font="SourceHanSansSC-Medium" fontSize={48} color="rgb(60, 211, 238)" />    
+                    <Container left={1024} top={1771} background={require("./images/tip.png")}>    
+                        <Text left={1024} top={1783} width={150} value={`${toFixed(this.state.直流电压1, 0)}V`} font="SourceHanSansSC-Bold" fontSize={41} align="center" />
+                    </Container>
+                    <Container left={1299} top={1771} background={require("./images/tip.png")}>    
+                        <Text left={1299} top={1783} width={150} value={`${toFixed(this.state.直流电流1, 0)}A`} font="SourceHanSansSC-Bold" fontSize={41} align="center" />
+                    </Container>
+                    <Container left={1588} top={1771} background={require("./images/tip.png")}>    
+                        <Text left={1588} top={1783} width={150} value={`${toFixed(this.state.逆变电流1, 0)}A`} font="SourceHanSansSC-Bold" fontSize={41} align="center" />
+                    </Container>
+                    <Progress left={1024} top={1850} width={221} height={14} value={this.state.直流电压1} />
+                    <Progress left={1299} top={1850} width={221} height={14} value={this.state.直流电流1} />
+                    <Progress left={1588} top={1850} width={221} height={14} value={this.state.逆变电流1} />
+                </Container>
+                <Container left={1933} top={1628} background={require("./images/box4.png")}>
+                    <Text left={2041} top={1690} value="直流电压" font="SourceHanSansSC-Medium" fontSize={48} color="rgb(60, 211, 238)" />    
+                    <Text left={2316} top={1690} value="直流电流" font="SourceHanSansSC-Medium" fontSize={48} color="rgb(60, 211, 238)" />    
+                    <Text left={2605} top={1690} value="逆变电流" font="SourceHanSansSC-Medium" fontSize={48} color="rgb(60, 211, 238)" />    
+                    <Container left={2041} top={1771} background={require("./images/tip.png")}>    
+                        <Text left={2041} top={1783} width={150} value={`${toFixed(this.state.直流电压1, 0)}V`} font="SourceHanSansSC-Bold" fontSize={41} align="center" />
+                    </Container>
+                    <Container left={2316} top={1771} background={require("./images/tip.png")}>    
+                        <Text left={2316} top={1783} width={150} value={`${toFixed(this.state.直流电流1, 0)}A`} font="SourceHanSansSC-Bold" fontSize={41} align="center" />
+                    </Container>
+                    <Container left={2605} top={1771} background={require("./images/tip.png")}>    
+                        <Text left={2605} top={1783} width={150} value={`${toFixed(this.state.逆变电流1, 0)}A`} font="SourceHanSansSC-Bold" fontSize={41} align="center" />
+                    </Container>
+                    <Progress left={2041} top={1850} width={221} height={14} value={this.state.直流电压1} />
+                    <Progress left={2316} top={1850} width={221} height={14} value={this.state.直流电流1} />
+                    <Progress left={2605} top={1850} width={221} height={14} value={this.state.逆变电流1} />
                 </Container>
             </Container>
         );
